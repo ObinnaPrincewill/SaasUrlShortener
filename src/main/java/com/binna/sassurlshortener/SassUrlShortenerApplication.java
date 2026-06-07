4538a808cf2690137f67b706182e1a0d4956961f
ObinnaPrincewill <psimon6717@gmail.com>
2026-06-07 03:17:50 +0100

    first commit

---
+++ src/main/java/com/binna/sassurlshortener/SassUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.binna.sassurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SassUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binna/sassurlshortener/SassUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binna.sassurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SassUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
